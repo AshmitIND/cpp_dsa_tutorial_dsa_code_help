03cea87a1c4dae134ae482998c823bffbff6fd13
AshmitIND <90672539+ashmit001@users.noreply.github.com>
2022-09-19 01:45:38 +0530

    Internal Commit

--- array/occurance_in_array.cpp
+++ array/occurance_in_array.cpp
@@ -8,17 +8,13 @@ int main(){
         cin>>arr[i];
     cout<<"enter the key"<<endl;
     cin>>key;
-    int s=0,e=size-1,mid=s+((e-s)/2),count=0;
+    int s=0,e=size-1,mid=s+((e-s)/2);
     while(s<=e){
-        if(arr[mid]==key)
-            count++;
-        else{
             if(arr[mid]<key)
                 s=mid+1;
             else
                 e=mid;
-        }
         mid=s+((e-s)/2);
     }
-    cout<<count<<endl;
+    cout<<(e-s)+1<<endl;
 }
